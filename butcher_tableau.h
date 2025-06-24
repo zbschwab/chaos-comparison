1c1ae645935d5fc6db14f01a90737c4ec41f1572
Z Schwab <zbajraktarischwab@gmail.com>
2025-06-23 20:46:14 -0400

    butcher tableau for RK45 in header file

---
+++ butcher_tableau.h
@@ -0,0 +1,39 @@
+// butcher tableau for RK45
+#ifndef BUTCHER_TABLEAU_H
+
+#define A2 0.2222222
+#define A3 0.3333333
+#define A4 0.7500000
+#define A5 1.0000000
+#define A6 0.8333333
+#define B12 0.2222222
+#define B13 0.0833333
+#define B14 0.5390625
+#define B15 -1.4166667
+#define B16 0.1504630
+#define B23 0.2500000
+#define B24 -1.8984375
+#define B25 6.7500000
+#define B26 -0.3125000
+#define B34 2.1093750
+#define B35 -5.4000000
+#define B36 0.8125000
+#define B45 1.0666667
+#define B46 0.1481481
+#define B56 0.0347222
+#define C1 0.1111111
+#define C3 0.4500000
+#define C4 0.3555556
+#define C5 0.0833333
+#define CH1 0.1044444
+#define CH3 0.4800000
+#define CH4 0.1422222
+#define CH5 0.0333333
+#define CH6 0.2400000
+#define CT1 0.0066667
+#define CT3 -0.0300000
+#define CT4 0.2133333
+#define CT5 0.0500000
+#define CT6 -0.2400000
+
+#endif
