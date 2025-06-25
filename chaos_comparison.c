67081538c33bf151cc066be553eee11f403d6130
Z Schwab <zbajraktarischwab@gmail.com>
2025-06-25 15:36:56 -0400

    lddp jacobian and debug

--- chaos_comparison.c
+++ chaos_comparison.c
@@ -2,15 +2,44 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <math.h>
+#include <cblas.h>
 
 #include "phys_math.h"
 
-int main(void) {
-    // initialize system constants
-    cons_dp_t c_dp = {1, 1, 1, 1};
+#define COMPUTE_STEPS 1000000
 
-    // initialize system states
-    // state_dp_t s_init_dp = {}   does it matter b/c we're iterating it anyways?
+int main(void) {
+    // lddp
+    cons_ddp_t* c_lddp = (cons_ddp_t*)malloc(sizeof(cons_ddp_t));
+    state_ddp_t* s_lddp = (state_ddp_t*)malloc(sizeof(state_ddp_t));;
+    double* lddp_jac = (double*)malloc(sizeof(double));
+
+    c_lddp->beta = 1;
+    c_lddp->omega0 = 1;
+    c_lddp->omegad = 1;
+    s_lddp->phi = 2;
+    s_lddp->omega = 2;
+
+    double t_step = 0.01;
+
+    lddp_jac = jac_lddp(s_lddp, c_lddp, t_step);
+
+    // test:
+    printf(" %lf ", lddp_jac);
+
+    // // initialize array to store convergence steps
+    // double lddp_lyp_con[6000];
+
+    // for (int i = 0; i < COMPUTE_STEPS; i++) {
+    //     lddp_lyp_con[i] = 
+    //     t_step = jac_lddp(s_lddp, c_lddp, t_step);
+    //     //printf(" %lf ", lddp_jac[i]);
+    // }
+
+    // qddp
+
+
+    // dp
 
 
 }
